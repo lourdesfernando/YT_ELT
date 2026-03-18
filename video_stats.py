f1e2a66d20c96f53193fe47c32c4f1e9f0ef60bb
Vishmanthi Fernando <vishmanthifernando@gmail.com>
2026-03-18 11:16:40 +0530

    loading data

--- video_stats.py
+++ video_stats.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import date
 import os 
 from dotenv import load_dotenv
 
@@ -79,8 +80,13 @@ def extract_video_data(video_id_lst):
     except requests.exceptions.RequestException as e:
         raise e
 
+def save_to_json(data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(data, json_outfile, indent=4, ensure_ascii=False)    
 
 if __name__ == "__main__":
     playlist_id = get_channel_playlist_id()
     video_ids = get_video_ids(playlist_id)
-    print(extract_video_data(video_ids))
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
